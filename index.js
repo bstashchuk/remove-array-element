97607e0ad0d843f92c729ce16f87771f77debe1b
Bogdan Stashchuk <43352618+bstashchuk@users.noreply.github.com>
2020-03-10 17:55:24 +0200

    Modify solution

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const _ = require('lodash');
 
 const inputArray = [1, 5, 6, 2, 3, 2, 5, 1];
-const elementToReplace = 2;
+const elementToRemove = 10;
 
 // // SOLUTION 1 - filter method. Removes all occurences of the element
-// const resultingArray = inputArray.filter(el => el !== elementToReplace);
+// const resultingArray = inputArray.filter(el => el !== elementToRemove);
 // console.log(resultingArray);
 // console.log(inputArray);
 
 // // SOLUTION 2 - slice and findIndex. Removes only first occurence
-// const index = inputArray.findIndex(el => el === elementToReplace);
-// const resultingArray = [...inputArray.slice(0, index), ...inputArray.slice(index + 1)];
+// const index = inputArray.findIndex(el => el === elementToRemove);
+// const resultingArray = index > -1 
+//     ? [...inputArray.slice(0, index), ...inputArray.slice(index + 1)]
+//     : [...inputArray];
 // console.log(resultingArray);
+// console.log(inputArray);
+
 
 // // SOLUTION 3 - lodash remove method. Without clone it mutates input array
-// const resultingArray = _.remove(_.clone(inputArray), el => el !== elementToReplace);
+// const resultingArray = _.remove(_.clone(inputArray), el => el !== elementToRemove);
 // console.log(resultingArray);
 // console.log(inputArray);
 
